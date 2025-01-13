3b0c2a90d07b4a40e5644a53713ef4b7151b50a7
Kartega Yazılım ve Danışmanlık A.Ş. <56034514+kartegagithub@users.noreply.github.com>
2025-01-13 15:44:53 +0300

    1.1.7

--- src/Service/serviceResult.tsx
+++ src/Service/serviceResult.tsx
@@ -12,6 +12,16 @@ export default class ServiceResult{
     performance: any
     encryptedData: any
     responseStatusCode: number = 0
+    MessageCallback?: (item: ServiceMessage) => void
+    Error(e: Error){
+        var message = new ServiceMessage();
+        message.code = "SYSERR";
+        message.type = 1;
+        message.message = e.message ?? "System error";
+        message.description = e.stack ?? e.message;
+        this.messages.push(message);
+        if(this.MessageCallback) this.MessageCallback(message);
+    }
     Fail(code: string, desc: string){
         if(!this.messages) this.messages = [];
 
@@ -20,6 +30,7 @@ export default class ServiceResult{
         message.type = 1;
         message.description = desc;
         this.messages.push(message);
+        if(this.MessageCallback) this.MessageCallback(message);
     }
     Warn(code: string, desc: string){
         if(!this.messages) this.messages = [];
@@ -29,6 +40,7 @@ export default class ServiceResult{
         message.type = 2;
         message.description = desc;
         this.messages.push(message);
+        if(this.MessageCallback) this.MessageCallback(message);
     }
     Success(code: string, desc: string){
         if(!this.messages) this.messages = [];
@@ -38,6 +50,7 @@ export default class ServiceResult{
         message.type = 3;
         message.description = desc;
         this.messages.push(message);
+        if(this.MessageCallback) this.MessageCallback(message);
     }
     SetData<T>(data: T){
         this.data = data;
