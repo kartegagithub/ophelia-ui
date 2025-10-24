006dda4b24d54a6df8e656cd83fbab29a356332f
Kartega Yazılım ve Danışmanlık A.Ş. <56034514+kartegagithub@users.noreply.github.com>
2025-10-24 13:52:14 +0300

    TextAreaInput revision

--- ophelia-next/src/Components/Inputs/TextAreaInput.tsx
+++ ophelia-next/src/Components/Inputs/TextAreaInput.tsx
@@ -22,6 +22,7 @@ const TextAreaInput: React.FC<TextAreaInputProps> = ({
   ...props
 }) => {
   const inputRef = createRef<HTMLTextAreaElement>();
+  const [text, setText] = React.useState<any>("");
   const onValueChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     checkHeight();
     if(onChange) onChange(e);
@@ -35,14 +36,30 @@ const TextAreaInput: React.FC<TextAreaInputProps> = ({
       else if(maxTrackedHeight) inputRef.current.style.height = maxTrackedHeight + "px";
     }
   }
+
+  useEffect(() => {
+    setText(props.value ?? props.defaultValue ?? "");
+  }, [props.value, props.defaultValue]);
+
   useEffect(() => {
     checkHeight();
   }, [inputRef, trackHeight, maxTrackedHeight])
 
+  const pureProps = (({
+      value,
+      defaultValue,
+      defaultChecked,
+      ...others
+    }) => others)(props);
+
   return (
     <textarea
-      {...props}
-      onChange={(e) => onValueChanged(e)}
+      defaultValue={text ?? ""}
+      {...pureProps}
+      onChange={(e) => {
+        setText(e.target.value);
+        onValueChanged(e)
+      }}
       ref={inputRef}
       className={`oph-textAreaInput peer ${errorClassName ? "error" : ""} ${labelType && labelType === "floating" ? "floating" : ""}`}
       rows={rows}
