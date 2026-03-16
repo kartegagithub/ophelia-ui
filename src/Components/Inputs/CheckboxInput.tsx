80d2b975500740369a56faf21b67f0471de82157
omersarisoy <omer.sarisoy@kartega.com>
2026-03-16 15:01:18 +0300

    checkbox unnecessary svg check removed

--- src/Components/Inputs/CheckboxInput.tsx
+++ src/Components/Inputs/CheckboxInput.tsx
@@ -86,18 +86,6 @@ const CheckboxInput: React.FC<
           {...props}
           style={style}
         />
-        <svg
-          className="oph-checkboxInput-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <polyline points="20 6 9 17 4 12"></polyline>
-        </svg>
         <label className="oph-checkboxInput-label" htmlFor={id + "-input"}>
           {label}
         </label>
